6899773ee79248602ee0e1b5384f09e31cc3508a
venkatesh344 <venkateshprasath900@gmail.com>
2022-01-25 11:40:20 +0530

    Modified New1

--- src/org/ne/Amma.java
+++ src/org/ne/Amma.java
@@ -10,7 +10,7 @@ public class Amma {
 		System.setProperty("webdriver.chrome.driver","D:\\eclipse files\\New1\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.facebook.com/");
+		driver.get("https://www.google.com/search?q=google&rlz=1C1GCEO_enIN988IN988&oq=google&aqs=chrome..69i57j0i271l3j69i65l2j69i60l2.1187j0j7&sourceid=chrome&ie=UTF-8");
 		
 		
 	}
